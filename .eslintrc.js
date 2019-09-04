58456fcaf77fb0de7661cd12ee09f77f7089f479
Brandon Dong <gasingdong@gmail.com>
2019-09-04 10:11:07 -0700

    add linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,24 @@
 module.exports = {
-  "extends": [
-    "airbnb",
-    "eslint:recommended",
-	  "plugin:prettier/recommended",
-  ],
-  "parserOptions": {
-	  "ecmaVersion": 6,
-	  "sourceType": "module",
-  },
-  "env": { 
-    "es6": true,
-  },
-  "globals": {
-    "console": "readonly",
-	  "document": "readonly",
-	  "window": "readonly",
-    "it": "readonly",
-    "localStorage": "readonly",
-  },
+	"extends": [
+		"airbnb",
+		"eslint:recommended",
+		"plugin:prettier/recommended",
+	],
+	"parserOptions": {
+		"ecmaVersion": 6,
+		"sourceType": "module",
+	},
+	"env": { 
+		"es6": true,
+	},
+	"rules": {
+		"no-console": "off",
+	},
+	"globals": {
+		"console": "readonly",
+		"document": "readonly",
+		"window": "readonly",
+		"it": "readonly",
+		"localStorage": "readonly",
+	},
 };
